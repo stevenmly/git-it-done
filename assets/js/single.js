6d41a629fa8b4cc0b363079d94629d07378cc8b7
Steven Ly <ly.steven16@gmail.com>
2021-01-15 14:41:46 -0600

    issues are shown for selected repository

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,13 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function () {
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+  getRepoIssues(repoName);
+  repoNameEl.textContent = repoName
+}
 
 var getRepoIssues = function(repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -72,4 +80,4 @@ var displayWarning = function(repo) {
 
 };
 
-getRepoIssues("facebook/react");
+getRepoName();

961a09b8cb4a0e883b542a80ce8ad969c0a49b0a
Steven Ly <ly.steven16@gmail.com>
2021-01-15 15:04:09 -0600

    Error handling for single.js file

--- assets/js/single.js
+++ assets/js/single.js
@@ -3,11 +3,21 @@ var limitWarningEl = document.querySelector("#limit-warning");
 var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoName = function () {
+  // grab repo name from url query string
   var queryString = document.location.search;
   var repoName = queryString.split("=")[1];
-  getRepoIssues(repoName);
-  repoNameEl.textContent = repoName
-}
+  
+  if(repoName) {
+    //display repo name on the page
+    repoNameEl.textContent = repoName;
+
+    getRepoIssues(repoName);
+  } 
+  else {
+    // if no repo was given, redirect to the homepage
+    document.location.replace("./index.html");
+  }
+};
 
 var getRepoIssues = function(repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -25,7 +35,7 @@ var getRepoIssues = function(repo) {
       });
     }
     else {
-      alert("There was a problem with your request!");
+      document.location.replace("./index.html");
     }
   });
 }
